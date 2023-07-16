7f9ef892b0f15480f34a1a66ecccf27e2e35d876
NageswararaoNilla <nillanageswararao075@gmail.com>
2023-07-16 04:28:05 +0000

    static page completed

--- src/components/AppointmentItem/index.js
+++ src/components/AppointmentItem/index.js
@@ -1,1 +1,28 @@
-// Write your code here
+import {format} from 'date-fns'
+
+import './index.css'
+
+const AppointmentItem = props => {
+  const {appointmentDetails} = props
+  const {title, date, isFavorite} = appointmentDetails
+
+  const dayFormat = format(date, 'dd MMMM yyyy, EEEE')
+
+  const starImgUrl = isFavorite
+    ? 'https://assets.ccbp.in/frontend/react-js/appointments-app/filled-star-img.png'
+    : 'https://assets.ccbp.in/frontend/react-js/appointments-app/star-img.png'
+
+  return (
+    <li className="list-container">
+      <div className="star-container">
+        <p className="title">{title}</p>
+        <button type="button" data-testid="star" className="star-btn">
+          <img src={starImgUrl} alt="star" className="star-img" />
+        </button>
+      </div>
+      <p className="day-style">Date: {dayFormat}</p>
+    </li>
+  )
+}
+
+export default AppointmentItem
--- src/components/Appointments/index.js
+++ src/components/Appointments/index.js
@@ -1,1 +1,81 @@
-// Write your code here
+import {Component} from 'react'
+
+import {v4} from 'uuid'
+
+import AppointmentItem from '../AppointmentItem'
+
+import './index.css'
+
+const initialList = [
+  {
+    id: v4(),
+    title: 'Dentist',
+    date: new Date(),
+    isFavorite: false,
+  },
+]
+
+class Appointments extends Component {
+  state = {appointmentsList: initialList, title: '', date: ''}
+
+  renderAppointmentItems = () => {
+    const {appointmentsList} = this.state
+    const listItems = appointmentsList.map(eachItem => (
+      <AppointmentItem appointmentDetails={eachItem} />
+    ))
+    return listItems
+  }
+
+  render() {
+    const {title, date} = this.state
+    return (
+      <div className="app-container">
+        <div className="appointment-container">
+          <h1 className="appointment-heading">Add Appointment</h1>
+          <div className="inputs-container">
+            <form className="form">
+              <label htmlFor="title-input" className="label">
+                TITLE
+              </label>
+              <input
+                type="text"
+                id="title-input"
+                placeholder="Title"
+                className="input-style"
+                value={title}
+              />
+              <br />
+              <label htmlFor="date-input" className="label">
+                DATE
+              </label>
+              <input
+                type="date"
+                id="date-input"
+                className="input-style"
+                value={date}
+              />
+              <br />
+              <button type="submit" className="add-btn">
+                Add
+              </button>
+            </form>
+          </div>
+          <hr className="line" />
+          <div className="appointments-added-container">
+            <div className="heading-container">
+              <h1 className="heading">Appointments</h1>
+              <button type="button" className="starred-btn">
+                Starred
+              </button>
+            </div>
+            <ul className="appointment-list">
+              {this.renderAppointmentItems()}
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Appointments
